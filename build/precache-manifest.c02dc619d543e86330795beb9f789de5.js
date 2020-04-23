50a9a732d0be808ae47eaf3519b38f020c0c4dc4
fguillot <florian.guillot@leoo.fr>
2020-04-23 17:30:49 +0200

    add build

---
+++ build/precache-manifest.c02dc619d543e86330795beb9f789de5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444cd9edeaba05bef666571258044564",
+    "url": "/index.html"
+  },
+  {
+    "revision": "469ee07038f289b54caf",
+    "url": "/static/css/main.a10fbcf0.chunk.css"
+  },
+  {
+    "revision": "ced8b979ab1fde37548e",
+    "url": "/static/js/2.9398fd00.chunk.js"
+  },
+  {
+    "revision": "a28fa4e65174afe65c40016b4eee293c",
+    "url": "/static/js/2.9398fd00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "469ee07038f289b54caf",
+    "url": "/static/js/main.1833bbcd.chunk.js"
+  },
+  {
+    "revision": "3bdf234c643ada94b20e",
+    "url": "/static/js/runtime-main.4d66bb4e.js"
+  },
+  {
+    "revision": "c942009b9406b986d84ba9acdd2ac413",
+    "url": "/static/media/Leoo.c942009b.png"
+  },
+  {
+    "revision": "c04f43b2f0b1ef5cf3334b965c0d3262",
+    "url": "/static/media/noonoo2.c04f43b2.svg"
+  }
+]);
